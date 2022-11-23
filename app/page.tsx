5d7f64a3a873151de81790a9c8049669c5aee6ed
Xavier Hazzard <xavier@hazzard.org.uk>
2022-11-23 22:33:01 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ export default function HomePage() {
                 })
             })
         } catch (err) {
-            setSong(err)
-            throw err
+            setSong(err.toString());
+            throw err;
         }
 
         //setSong('');
